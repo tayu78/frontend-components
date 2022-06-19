1d0348e943f479c9a0eeddc32b73b521877edbc4
Yuya Satake <sata.yu.7878@gmail.com>
2022-06-19 00:00:15 -0700

    add header and form

--- BookList/tailwind.config.js
+++ BookList/tailwind.config.js
@@ -2,7 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '2rem'
+    }
   },
   plugins: [],
 }
---
+++ memo.md
@@ -0,0 +1,7 @@
+# memo
+## HTML
+### input
+- `name` : specifies the name od an <input> element
+
+### svg
+- 塗りつぶしたい時は`fill`プロパティを使用できる([参考](https://css-tricks.com/almanac/properties/f/fill/))
