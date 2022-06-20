18d828ddb56c18209832f944a664b391928aae9e
Yuya Satake <sata.yu.7878@gmail.com>
2022-06-19 23:44:21 -0700

    add alert function

--- BookList/src/index.js
+++ BookList/src/index.js
@@ -34,8 +34,27 @@ class UI{
             <td>${book.author}</td>
             <td>${book.isbn}</td>
             `
+
             bookList.appendChild(row)
         })
+    }
+
+    static removeInput() {
+        const ids = ["title","author","isbn"]
+        ids.forEach((id) => {
+            document.getElementById(id).value = ""
+        })
+        
+    }
+
+    static showAlert(message,caseOfAlert) {
+        const div = document.createElement("div");
+        div.className = `alert-${caseOfAlert}`
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector(".container")
+        const form = document.getElementById("bookForm")
+        container.insertBefore(div, form)
+        setTimeout(()=>container.removeChild(div),3000)
     }
 
 }
@@ -53,9 +72,14 @@ document.getElementById("bookForm").addEventListener("submit", (e) => {
     const title = document.getElementById("title").value;
     const author = document.getElementById("author").value;
     const isbn = document.getElementById("isbn").value;
-    const book = new Book(title, author, isbn);
-    const books = [book]
-    UI.addBookList(books);
+    if (title && author && isbn) {
+        const book = new Book(title, author, isbn);
+        const books = [book]
+        UI.addBookList(books)
+        UI.removeInput()
+    } else { 
+        UI.showAlert("plese fill in  all input field", "danger")
+    }
 })
 
 // // event remove a book
